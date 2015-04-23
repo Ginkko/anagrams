20b6ac27b4017f2c913c08f02b06d5cf46c0ad47
Alec Arme & Toby Alden <student@epicodus.com>
2015-04-23 12:25:19 -0700

    Refactored anagrams.rb. Clarified variable names.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,13 +1,23 @@
 class String
-  define_method(:anagrams) do |word_to_compare|
+  define_method(:anagrams) do |words_to_compare|
+
+    final_anagram_words = []
 
     master_word_letters = self.split("")
-    word_to_compare_letters = word_to_compare.split("")
+    words_to_compare_array = words_to_compare.split(" ")
 
-    if master_word_letters.sort().eql?(word_to_compare_letters.sort())
-      word_to_compare_letters.join()
-    else
+    words_to_compare_array.each() do |word|
+      word_to_compare_letters = word.split("")
+
+      if master_word_letters.sort().eql?(word_to_compare_letters.sort())
+        final_anagram_words.push(word)
+      end
+    end
+
+    if final_anagram_words.empty?()
       "No anagrams found."
+    else
+      final_anagram_words.join(" ")
     end
 
   end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,8 +3,13 @@ require('pry')
 require('anagrams')
 
 describe("String#anagrams") do
+
   it("Accepts input of two strings and returns the second string if it is an anagram of the first string.") do
     expect("fox".anagrams("xof")).to(eq("xof"))
+  end
+
+  it("Accepts input of two strings and returns any anagrams of the first string found as whole words in the second.") do
+    expect("fox".anagrams("xof car ofx")).to(eq("xof ofx"))
   end
 
 end
